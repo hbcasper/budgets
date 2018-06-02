133eebc4a8b89d2b6516e15545bb959a99e4ee06
Heidi Castillo <hb.casper@gmail.com>
2018-06-02 15:40:24 -0500

    repair

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Main from './app/components/Main';
-  firebase.initializeApp(config);
 //import firebase from 'firebase':
 
 export default class App extends React.Component {
