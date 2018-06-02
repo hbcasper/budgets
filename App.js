6e61d5c79fad42c84e2302d49e1580f13eff563e
Heidi Castillo <hb.casper@gmail.com>
2018-06-02 16:04:09 -0500

    Connected firebase

--- App.js
+++ App.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import Main from './app/components/Main';
-//import firebase from 'firebase':
+import firebase from "firebase";
 
 export default class App extends React.Component {
 
+  componentWillMount(){
+
+	var config = {
+    apiKey: "AIzaSyAI5dcM5PkDmCLlthwtDWVKAfW2eWr_wx8",
+    authDomain: "budgets-d6ff5.firebaseapp.com",
+    databaseURL: "https://budgets-d6ff5.firebaseio.com",
+    projectId: "budgets-d6ff5",
+    storageBucket: "budgets-d6ff5.appspot.com",
+    messagingSenderId: "901207872688"
+  };
+  firebase.initializeApp(config);
+
+  console.log(firebase)
+
+
+
+}
 
 
   render() {
