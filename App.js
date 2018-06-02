d44bd7d8379932b4c693938cf28ed1ce03c5f7c4
Heidi Castillo <hb.casper@gmail.com>
2018-06-02 15:40:05 -0500

    Repair

--- App.js
+++ App.js
@@ -1,28 +1,9 @@
 import React from 'react';
 import Main from './app/components/Main';
-import firebase from 'firebase':
-
-class App extends React.Component {
-  
-
-componentWillMount(){
-
-	var config = {
-    apiKey: "AIzaSyAI5dcM5PkDmCLlthwtDWVKAfW2eWr_wx8",
-    authDomain: "budgets-d6ff5.firebaseapp.com",
-    databaseURL: "https://budgets-d6ff5.firebaseio.com",
-    projectId: "budgets-d6ff5",
-    storageBucket: "budgets-d6ff5.appspot.com",
-    messagingSenderId: "901207872688"
-  };
-
   firebase.initializeApp(config);
+//import firebase from 'firebase':
 
-
-
-}
-
-
+export default class App extends React.Component {
 
 
 
